a23dbcc12a419536e4fb1c4c0e99624051c80479
aulia-rosyida <rosyidaaulia24@gmail.com>
2024-06-06 11:27:50 +0700

    feat: edit book data implement

--- src/handler.js
+++ src/handler.js
@@ -1,6 +1,72 @@
 const { nanoid } = require("nanoid");
 const books = require("./books");
 
+const editDataBookHandler = (request, h) => {
+  const { bookId } = request.params;
+  const {
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    reading,
+  } = request.payload;
+
+  const updatedAt = new Date().toISOString();
+  if (!name) {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal memperbarui buku. Mohon isi nama buku",
+    });
+
+    response.code(400);
+
+    return response;
+  } else if (readPage > pageCount) {
+    const response = h.response({
+      status: "fail",
+      message:
+        "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount",
+    });
+
+    response.code(404);
+    return response;
+  } else {
+    const index = books.findIndex((book) => book.id === bookId);
+
+    if (index !== -1) {
+      books[index] = {
+        ...books[index],
+        name,
+        year,
+        author,
+        summary,
+        publisher,
+        pageCount,
+        readPage,
+        reading,
+        updatedAt,
+      };
+
+      const response = h.response({
+        status: "success",
+        message: "Buku berhasil diperbarui",
+      });
+      response.code(200);
+      return response;
+    } else {
+      const response = h.response({
+        status: "fail",
+        message: "Gagal memperbarui buku. Id tidak ditemukan",
+      });
+      response.code(400);
+      return response;
+    }
+  }
+};
+
 const getDetailBookHandler = (request, h) => {
   const { bookId } = request.params;
 
@@ -144,4 +210,9 @@ const addBookHandler = (request, h) => {
   }
 };
 
-module.exports = { addBookHandler, getAllBooksHandler, getDetailBookHandler };
+module.exports = {
+  addBookHandler,
+  getAllBooksHandler,
+  getDetailBookHandler,
+  editDataBookHandler,
+};
