1ad8e2926d928b8ef71a3ed162e5931c51cede3d
aulia-rosyida <rosyidaaulia24@gmail.com>
2024-06-11 13:39:17 +0700

    feat: config eslint airbnb-base

---
+++ eslint.config.cjs
@@ -0,0 +1,30 @@
+const globals = require('globals');
+const pluginJs = require('@eslint/js');
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+module.exports = [
+  ...compat.extends('airbnb-base'),
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      ecmaVersion: 12,
+    },
+    rules: {
+      noConsole: 'off',
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        myCustomGlobal: 'readonly',
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+];
